85faa02ee6fe13a4b60c41aa4174eecc56d8abf8
teakyzone <tutiqka@live.com>
2025-08-05 18:21:37 +0100

    assignment push 3

--- client/src/main.js
+++ client/src/main.js
@@ -1,56 +1,69 @@
-console.log("main.js here, reporting live from the scene of the new digital camera form");
+console.log(
+  "main.js here, reporting live from the scene of the new digital camera form"
+);
 
-const userDigitalCameraForm = document.getElementById("user_digital_camera_form");
+const userDigitalCameraForm = document.getElementById(
+  "user_digital_camera_entry_form"
+);
 
 userDigitalCameraForm.addEventListener("submit", handleSubmit);
 
-function handleSubmit(event) {
+async function handleSubmit(event) {
   event.preventDefault();
 
-  // I need to get the form data out of my form with our two helpful lines:
   const data = new FormData(userDigitalCameraForm);
   const formValues = Object.fromEntries(data);
   console.log(formValues);
 
-  // Now what I need to do is send that form data to my server
-  // I'll do this with a fetch() request to our server endpoint, which uses the http POST method, and has some options attached to it.
-  fetch("http://localhost:8222/digitalcameras", {
-    method: "POST", // we tell our fetch request which HTTP method we're using
-    body: JSON.stringify(formValues), // we need to attach a body of information. JSON.stringify does exactly that - it makes the data into a string.
+  const serverResponse = await fetch("http://localhost:8222/digitalcameras", {
+    method: "POST",
+    body: JSON.stringify(formValues),
     headers: {
       "Content-Type": "application/json",
-    }, // json stands for Javascript Object Notation. We're explaining the format of what we're sending.
+    },
   });
 
+  console.log(serverResponse);
+  const parsedResponse = await serverResponse.json();
+  console.log(parsedResponse);
+
+  if (parsedResponse.rowCount == 1) {
+    getAllDigitalCameras();
+    userDigitalCameraForm.reset();
+  }
+}
+
+const digitalCameraContainer = document.getElementById(
+  "digital_camera_container"
+);
+
+async function getAllDigitalCameras() {
+  digitalCameraContainer.innerHTML = "";
+  const response = await fetch(
+    "https://week-9-assignment-server.onrender.com/digitalcameras"
+  );
+  const digitalCameraData = await response.json();
+  console.log("The digital camera data is:", digitalCameraData);
+
+  digitalCameraData.forEach((digitalcamera) => {
+    console.log(digitalcamera);
+
+    const brand = document.createElement("h3");
+    const series = document.createElement("p");
+    const model = document.createElement("p");
+    const releaseYear = document.createElement("p");
+    const img = document.createElement("img");
+    const alt = document.createElement("p");
+
+    brand.textContent = digitalcamera.camera_brand;
+    series.textContent = digitalcamera.camera_series;
+    model.textContent = digitalcamera.camera_model;
+    releaseYear.textContent = digitalcamera.release_year;
+    img.src = digitalcamera.img_source;
+    alt.textContent = digitalcamera.img_alt;
+
+    digitalCameraContainer.append(brand, series, model, releaseYear, img, alt);
+  });
 
   getAllDigitalCameras();
-  userDigitalCameraForm.reset();
 }
-
-const digitalCameraContainer = document.getElementById("digital_camera_container")
-
-async function getAllDigitalCameras (){
-  digitalCameraContainer.innerHTML ="";
-  const response = await fetch("//insert my render html")
-;
-const digitalCameraData = await response.json();
-console.log("The digital camera data is:", digitalCameraData);
-
-digitalCameraData.forEach((digitalcamera)
-=> {
-  //console.log(digitalcamera);
-
-  const cameraBrand = document.createElement("h3")
-  //etc etc
-
-  //attatched to object in database from console log to loop digitalcamera.camera_brand
-
-  cameraBrand.textContent = digitalcamera.camera_brand;
-
-  digitalCameraContainer.append(cameraBrand, cameraModel ///etc etc)]]
-
-  // etc etc 
-)});
-}
-
-getAllDigitalCameras();
--- server/server.js
+++ server/server.js
@@ -1,49 +1,41 @@
-import express, { response } from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-import pg from "pg" 
+import express, { response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pg from "pg";
 
-const app = express()   
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 dotenv.config();
 
 const PORT = 8222;
 const db = new pg.Pool({
-    connectionString: process.env.DATABASE_CONNECTION_STRING,
-
+  connectionString: process.env.DATABASE_CONNECTION_STRING,
 });
 
 console.log("my very secret key is", process.env.MY_VERY_SECRET_KEY);
 
-
-
-app.get("/", function (req, res){
-    res.send("this is my root route hun")
-
+app.get("/", function (req, res) {
+  res.send("this is my root route hun");
 });
 
 app.get("/digitalcameras", async function (req, res) {
-    const allDigitalCameras = await db.query('SELECT * FROM digital_cameras');
+  const allDigitalCameras = await db.query("SELECT * FROM digital_cameras");
 
-    console.log(allDigitalCameras.rows);
-    res.json(allDigitalCameras);
+  console.log(allDigitalCameras.rows);
+  res.json(allDigitalCameras);
 });
 
-console.log("Is this working")
+console.log("Is this working");
 
-app.get("/digitalcamerasquery", async function (req, res)
-{ const digitalCamerasQuery = await db.query(
+app.get("/digitalcamerasquery", async function (req, res) {
+  const digitalCamerasQuery = await db.query(
     `SELECT * FROM digitalcameras WHERE release_year = ${request.query.year}`
-)
-    
-    console.log("The request.query: ", req.query);
-    console.log("The request.query.year: ", rewq.query.year)
-}
-);
+  );
+});
 
-response.json(digitalCamerasQuery.rows);
+res.json(digitalCamerasQuery.rows);
 
-app.listen(8222, function(){
-    console.log('server is running on port: 8222')
+app.listen(8222, function () {
+  console.log("server is running on port: 8222");
 });
