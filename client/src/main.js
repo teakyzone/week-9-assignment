6be8090c7610a3676a54088392ccbd5f2211948d
teakyzone <tutiqka@live.com>
2025-08-08 18:10:22 +0100

    done

--- client/src/main.js
+++ client/src/main.js
@@ -16,17 +16,14 @@ async function handleSubmit(event) {
   const formValues = Object.fromEntries(data);
   console.log(formValues);
 
-  const serverResponse = await fetch(
-    "https://week-9-assignment-server.onrender.com/digitalcameras",
-    {
-      //    http://localhost:8222/digitalcameras
-      method: "POST",
-      body: JSON.stringify(formValues),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const serverResponse = await fetch("http://localhost:8222/digitalcameras", {
+    //    http://localhost:8222/digitalcameras tried all ways of combining these and it doesnt work https://week-9-assignment-server.onrender.com/digitalcameras i get server errors
+    method: "POST",
+    body: JSON.stringify(formValues),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 const digitalCameraContainer = document.getElementById(
@@ -39,6 +36,26 @@ async function getAllDigitalCameras() {
   );
   const digitalCameraData = await response.json();
   console.log("The digital camera data is:", digitalCameraData);
+
+  digitalCameraData.forEach((digitalcamera) => {
+    //console.log(digitalcamera);
+
+    const brand = document.createElement("h3");
+    const series = document.createElement("p");
+    const model = document.createElement("p");
+    const releaseYear = document.createElement("p");
+    const img = document.createElement("img");
+    const alt = document.createElement("p");
+
+    brand.textContent = digitalcamera.camera_brand;
+    series.textContent = digitalcamera.camera_series;
+    model.textContent = digitalcamera.camera_model;
+    releaseYear.textContent = digitalcamera.release_year;
+    img.src = digitalcamera.img_source;
+    alt.textContent = digitalcamera.img_alt;
+
+    digitalCameraContainer.append(brand, series, model, releaseYear, img, alt);
+  });
 }
 
 getAllDigitalCameras();
